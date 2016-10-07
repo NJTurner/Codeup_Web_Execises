b9493eabdb192460e3a01c54cedf2c05d131898a
Nick Turner <dimesteaksNT@gmail.com>
2016-10-07 14:30:46 -0500

    99 bottles exercise JS files added final notes

--- JS/99bottles.js
+++ JS/99bottles.js
@@ -6,9 +6,36 @@
 var numberofBottles = 99;
 var bottlesLeft = numberofBottles;
 
-console.log(numberofBottles + " bottles of beer on the wall, " + numberofBottles + " bottle of beer.  Take one down pass it around");
+console.log(numberofBottles + " bottles of beer on the wall, " + numberofBottles + " bottles of beer.  Take one down pass it around");
 do{
     bottlesLeft = bottlesLeft - 1;
-    console.log(bottlesLeft + " bottles of beer on the wall, " + bottlesLeft + " bottle of beer.  Take one down pass it around")
+    console.log(bottlesLeft + " bottles of beer on the wall, " + bottlesLeft + " bottles of beer.  Take one down pass it around");
 } while (bottlesLeft >= 2);
 console.log("no more bottles of beer on the wall.  Go buy some.");
+// 1 bottles of beer smh
+
+// //Ryans Code
+// var bottlesOfBeer = 99;
+//
+// while(bottlesOfBeer > 0) {
+//     //if bottlesOfBeer is 1, then use "bottle" instead of bottles;
+//     if(bottlesOfBeer == 2) {
+//         console.log(bottlesOfBeer + " bottles of beer on the wall.");
+//         console.log(bottlesOfBeer + " bottles of beer!");
+//         console.log("Take one down and pass it around...");
+//         console.log((bottlesOfBeer - 1) + " bottles of beer on the wall.");
+//     } else if (bottlesOfBeer == 1) {
+//         console.log(bottlesOfBeer + " bottle of beer on the wall.");
+//         console.log(bottlesOfBeer + " bottle of beer!");
+//         console.log("Take one down and pass it around...");
+//         console.log("no more bottles of beer on the wall.");
+//     } else {
+//         console.log(bottlesOfBeer + " bottles of beer on the wall.");
+//         console.log(bottlesOfBeer + " bottles of beer!");
+//         console.log("Take one down and pass it around...");
+//         console.log((bottlesOfBeer - 1) + " bottles of beer on the wall.");
+//     }
+//     bottlesOfBeer--;
+// }
+//
+// console.log("somebody go to the store!");
