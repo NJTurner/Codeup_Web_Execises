02b791b3ad72aed31e28274c7c785a4b02efb483
Nick Turner <dimesteaksNT@gmail.com>
2016-10-10 17:54:26 -0500

    user interaction exercise

---
+++ JS/function.js
@@ -0,0 +1,59 @@
+/*** Created by NicholasTurner on 10/10/16.*/
+'use strict';
+
+// //methods, a type of function
+// isNan();
+// parseInt();
+// parseFloat();
+// .toString();
+// .repeat();
+// Math.floor();
+// Math.ceil();
+// console.log();
+// alert();
+// function getRandomOddNumber(start,end) {
+//     do{
+//     var rand = Math.floor((Math.random()*(end - start)+ start));
+//     } while (rand % 2 == 0);
+//     return rand;
+// }
+
+// isOdd(number);
+//
+// function isOdd(number) {
+//     var result = number % 2;
+//     if (result != 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+
+// var square = function(number) {
+//     return number * number;
+// }
+//
+// function square(number) {
+//     var result = number * number;
+//     return result;
+// }
+//
+// function square(number) {
+//     return number * number;
+// }
+//
+// function isEven(input) {
+//     var remainder= input % 2;
+//     if(remainder === 0) {
+//     return true;
+//     } else {
+//     return false;
+//     }
+//  }
+//
+//  function sum (a, b) {
+//      return a + b;
+//  }
+//
+//  console.log(isEven(sum(2, 3))); // = console.log(false)
---
+++ JS/functions_exercise.js
@@ -0,0 +1,84 @@
+/*** Created by NicholasTurner on 10/10/16.*/
+
+"use strict";
+
+var myNameIs = 'Nick'; // TODO: Fill in your name here.
+
+// TODO:
+// Create a function called 'sayHello' that takes a parameter 'name'.
+// When called, the function should log a message that says hello from the passed in name.
+
+// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+
+// Don't modify the following line
+// It generates a random number between 1 and 100 and stores it in random
+var random = Math.floor((Math.random()*100)+1);
+
+// TODO:
+// Create a function called 'isOdd' that takes a number as a parameter.
+// The function should use the ternary operator to log a message.
+// The log should tell the number passed in and whether it is odd or not.
+
+// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+
+//1.
+function sayHello(name) {
+    return "Hello, my name is " + name;
+}
+console.log(sayHello(myNameIs)); //add console log to read out
+
+//2.
+var message;
+function isOdd(number) {
+    var result = number % 2;
+    (result !== 0) ? message = number + " is an odd number": message = number + " is not an odd number";
+    return message;
+    // if (result !== 0) {
+    //     message = number + " is an odd number";
+    //     return message;
+    // } else {
+    //     message = number + " is not an odd number";
+    //     return message;
+    // }
+}
+
+function isNumeric(input) {
+    return !isNaN(input)
+}
+
+
+function add(a, b) {
+    if (isNumeric(a) && (isNumeric(b))) {
+        return a + b;
+    } else {
+        return "parameters must both be numeric";
+    }
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Using these functions and only these functions
+// write square(number)
+//
+// write sumOfSquares(a, b)
+// squares a, squares b
+// returns the sum of those numbers
+
+function square(number) {
+    return multiply(number, number);
+}
+
+function sumOfSquares(a, b){
+    return add(square(a), square(b));
+}
+
