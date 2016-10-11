d65733a199e5ab2b57b32757711d7ff8454c4547
Nick Turner <dimesteaksNT@gmail.com>
2016-10-11 16:02:04 -0500

    addingarrays and iterating arrays work

---
+++ JS/arrays.js
@@ -0,0 +1,18 @@
+/**Created by NicholasTurner on 10/11/16.*/
+'use strict';
+
+var chores = ['do the dishes', 'laundry','sweep the kitchen floor', 'mop the bathroom floor'];
+var grades = [100, 90, 80,60];
+
+grades[grades.length - 1];
+60
+
+grades[1];
+90
+grades[0] = 50; //reassign elements of an array
+grades;
+[50, 90, 80, 60];
+
+grades[4] = 75; //add another value to array
+grades;
+[50, 90, 80, 60, 75];
